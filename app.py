cbfac01ce799419a99d52fd1e32e4eaf21cd0cb8
eaglewolves304 <eaglewolves304@gmail.com>
2026-06-08 01:21:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # ======================
 import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = "8581143855"
+CHAT_ID = os.getenv("CHAT_ID")
 
 TELNYX_API_KEY = os.getenv("TELNYX_API_KEY")
 
